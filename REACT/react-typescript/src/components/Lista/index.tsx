426cf9a0b94e747798e331853a766ae850f51184
Diego Brito <diegorbrito9@gmail.com>
2022-02-01 11:11:07 -0300

    fim da aula 'introduzindo estados' [react-typescript | alura]

--- REACT/react-typescript/src/components/Lista/index.tsx
+++ REACT/react-typescript/src/components/Lista/index.tsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import Item from '../Item';
 import style from './style.module.scss';
 
 function Lista() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { title: 'react', time: '02:00:00' },
     { title: 'javascript', time: '01:00:00' },
     { title: 'typescript', time: '03:00:00' },
-  ];
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => setTasks([...tasks, { title: 'vue', time: '02:00:00' }])}>
+        Estudos do dia
+      </h2>
       <ul>
         {tasks.map((task) => (
           <Item {...task} key={task.title} />
