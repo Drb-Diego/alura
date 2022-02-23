dda233db177443bb349135b4fade7eb1085a3c42
Diego Brito <diegorbrito9@gmail.com>
2022-02-22 22:56:16 -0300

    [ next ] fim da aula 'iniciando o projeto'

---
+++ nextjs/src/pages/faq.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FAQPage() {
+  return (
+    <div>
+      <h1>Paginas de perguntas faq</h1>
+    </div>
+  )
+}
+
+export default FAQPage;
---
+++ nextjs/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Alura Cases - Home Page</h1>
+      <a href="/faq">Ir para o faq</a>
+    </div>
+  );
+}
+
+export default HomePage;
